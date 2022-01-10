9779e471f91020495d02941b7110b3f0c973c2ac
oybek703 <hhoybek@gmail.com>
2022-01-10 20:22:36 +0500

    add to cart implemented

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.get_products, name='products'),
-    path('products/<str:pk>', views.get_product, name='product'),
-    path('cart', views.add_to_cart, name='cart')
+    path('products/<str:pk>', views.get_product, name='product')
 ]
