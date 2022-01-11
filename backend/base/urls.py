b26ee297e0519566e1bae0b3d70f6fae48924869
oybek703 <hhoybek@gmail.com>
2022-01-11 16:33:02 +0500

    user serializer added

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView
-)
+
 
 urlpatterns = [
-    path('users/login', TokenObtainPairView.as_view(), name='login_user'),
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='login_user'),
+    path('users/profile', views.get_user_profile, name='user_profile'),
+    path('routes', views.get_routes, name='all_routes'),
     path('products/', views.get_products, name='products'),
-    path('products/<str:pk>', views.get_product, name='product')
+    path('products/<str:pk>', views.get_product, name='product'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,14 +1,31 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
-def get_cart_id(request):
-    cart_id = request.session.session_key
-    if not cart_id:
-        cart_id = request.session.create()
-    return cart_id
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        '/api/products',
+        '/api/users/login'
+    ]
+    return Response(routes)
 
 
 @api_view(['GET'])
@@ -22,4 +39,11 @@ def get_products(request):
 def get_product(request, pk):
     product = Product.objects.get(pk=pk)
     serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
