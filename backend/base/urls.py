df49341d960dd6fca5626f6bace7362320360fea
oybek703 <hhoybek@gmail.com>
2022-01-11 13:04:31 +0500

    jwt added as default auth system

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
 
 urlpatterns = [
+    path('users/login', TokenObtainPairView.as_view(), name='login_user'),
     path('products/', views.get_products, name='products'),
     path('products/<str:pk>', views.get_product, name='product')
 ]
