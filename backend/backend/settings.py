fdf239470e33ad7ce070d405c21854250c3bc7a1
oybek703 <hhoybek@gmail.com>
2022-01-22 17:10:46 +0500

    basic docker containerization done but not completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,15 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f8ov0%mo20stw9w3sk7)bk*1vacwmgpso7t()9m38q)0^f76(z'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # PAYPAL CLIENT ID
 PAYPAL_CLIENT_ID = 'AXx-psDLDCzl-Dl2auLIbAbJIR8LTPA1qTaSqI5HvPVUN0jqFPoXDyyyciVyUwnPU7Sf08Ro91C1sx_q'
-
-ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -84,8 +86,6 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -124,13 +124,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'proshop',
-            'USER': 'postgres',
-            'PASSWORD': '123456',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "postgres"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "123456"),
+        "HOST": os.environ.get("SQL_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("SQL_PORT", "5432")
+    }
 }
 
 # Password validation
@@ -174,7 +174,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/images'
 ]
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -184,7 +184,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # CORS Settings
 CORS_ALLOW_ALL_ORIGINS = True
